eff36359d151fe89219c7a9a45773f5aa5351e9c
James <Natasan2121@gmail.com>
2023-10-22 17:32:57 +0700

    complete closure assignment

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,19 @@
 function matchAction(x) {
   // Start coding here
+  let objectFunction = {}
+  objectFunction.addByX = function(y){let a = x+y ;return a}
+  objectFunction.subtractByX = function(y){let a = y-x;return a}
+  return objectFunction
 }
 
 // Uncomment โค้ดด้านล่างเพื่อดูผลลัพธ์ของการทำงาน
 
-// const addByTwo = matchAction(2);
-// console.log(addByTwo.addByX(1)); // => should return 3
-// console.log(addByTwo.addByX(2)); // => should return 4
-// console.log(addByTwo.addByX(3)); // => should return 5
+ const addByTwo = matchAction(2);
+ console.log(addByTwo.addByX(1)); // => should return 3
+ console.log(addByTwo.addByX(2)); // => should return 4
+ console.log(addByTwo.addByX(3)); // => should return 5
 
-// const subtractByThree = matchAction(3);
-// console.log(subtractByThree.subtractByX(4)); // => should return 1
-// console.log(subtractByThree.subtractByX(6)); // => should return 3
-// console.log(subtractByThree.subtractByX(8)); // => should return 5
+ const subtractByThree = matchAction(3);
+ console.log(subtractByThree.subtractByX(4)); // => should return 1
+ console.log(subtractByThree.subtractByX(6)); // => should return 3
+ console.log(subtractByThree.subtractByX(8)); // => should return 5
